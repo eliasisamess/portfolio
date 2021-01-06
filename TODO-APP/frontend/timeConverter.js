4dbd4dd74851a450676c30cc4a471349bcd7c87a
Elias Puukari <elias.puukari@gmail.com>
2021-01-06 23:14:05 +0200

    Relevant parts of frontend

---
+++ TODO-APP/frontend/timeConverter.js
@@ -0,0 +1,50 @@
+// !!!! PORTFOLIO NOTES !!!!
+
+// These utility scripts return hours and minutes in proper formats for different uses in frontend.
+
+// !!!! ORIGINAL CODE STARTS HERE !!!!
+
+module.exports = {
+  // takes total_minutes as parameter and converts them to [h min] format, for example param 80
+  // will return string "1h 20min"
+  minutesToTitle: (total_minutes) => {
+    let string = "";
+    let hours = parseInt(total_minutes / 60);
+    let minutes = total_minutes % 60;
+    if (hours > 0) {
+      string += hours + "h ";
+    }
+    if (minutes > 0) {
+      string += minutes + "min";
+    }
+
+    return string;
+  },
+  minutesToSlider: (total_minutes) => {
+    let string = "";
+    let hours = parseInt(total_minutes / 60);
+    let minutes = total_minutes % 60;
+    if (hours > 0) {
+      string += hours + "h";
+    }
+    if (minutes > 0) {
+      string += minutes + "min";
+    } else if (minutes === 0) {
+      string += "00min";
+    }
+    return string;
+  },
+  minutesToSubtask: (total_minutes) => {
+    let string = "";
+    let hours = parseInt(total_minutes / 60);
+    let minutes = total_minutes % 60;
+    if (hours > 0) {
+      string += hours + "h ";
+    }
+    if (minutes > 0) {
+      string += minutes + " min";
+    }
+
+    return string;
+  },
+};
